9d4ec7b47a023112b38092807923208d86e53c3e
Daniel Jacon <danielcamillo2020@gmail.com>
2023-01-03 23:02:11 -0300

    Adicionada a sessão principal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import "./styles.scss";
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <main className="container-content main-section">
+        <div>
+          <h1>Para qual estado você deseja ir?</h1>
+          <input type="text" placeholder="Pesquisar" />
+          <Button text="buscar" variant="contained" />
+        </div>
+      </main>
     </>
   );
 };
